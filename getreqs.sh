b935cbc7940d0f8fbe66f61ca092145c363a7e8c
Øyvind Bergerud <oyvind.bergerud@gjensidige.com>
2020-12-06 23:34:27 +0100

    init

---
+++ getreqs.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+wget https://raw.githubusercontent.com/SecurityRAT/Security-Requirements/master/requirements.sql /var
+wget https://raw.githubusercontent.com/SecurityRAT/Security-Requirements/master/OWASP-ASVS-4.0.1.sql /var
